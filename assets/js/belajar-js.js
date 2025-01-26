2a896ea1d9d3d9a926fbff19eda317a631a0d4f4
daffa6690 <daffadaffamdf@gmail.com>
2025-01-26 14:47:37 +0700

    update

---
+++ assets/js/belajar-js.js
@@ -0,0 +1,36 @@
+let greeting = "Morn Sayang"; // type data string dengan petik 2 ("")
+let ucapanSelamat = "Congrats Sayang"; // type data string dengan petik 1 ('')
+
+let age = 21; // type data number
+
+// alert("20" + "30"); // concatenation
+// alert(123 + 456); // math operation
+
+let lampIsOn = true; // type data boolean
+
+let boxContain = undefined; // type data undefined
+
+let bagContain = null; // type data null
+
+let person = {
+  // type data object
+  firstName: "Mohammad",
+  lastName: "Daffa",
+  address: "Tangsel",
+};
+
+let pesertaBootcamp = ["Ahmad Ainul", "Alex Josua", "Husni Fatah"]; // array of strings
+
+let bilanganPrima = [2, 3, 5, 7, 11, 13]; // array of number
+
+let pesertaBootcampObject = [
+  // array of objects
+  {
+    firstName: "Naufal",
+    lastName: "Ilyasa",
+  },
+  {
+    firstName: "Rizal",
+    lastName: "Khudori",
+  },
+];
